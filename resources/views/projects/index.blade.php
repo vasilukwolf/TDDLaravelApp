607efd9dcc07b75f43c3ffe6f8ef53ba69fe5a39
Jimmy Basiluk <vasilukwolf@gmail.com>
2019-11-09 15:35:51 -0500

    Add projects dashboard style

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,13 +3,14 @@
 <div class="flex items-center mb-4">
     <a href="/projects/create" class="button">New projects</a>
 </div>
-<div class="flex">
+<div class="flex flex-wrap -mx-3">
     @forelse($projects as $project)
-
-    <div class="bg-white mr-4 rounded shadow w-1/3 p-5">
+    <div class="w-1/3 px-4 pb-6">
+    <div class="bg-white rounded shadow p-5">
         <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
         <div class="text-grey">{{ Str::limit($project->description,100) }}</div>
     </div>
+  </div>
     @empty
     <div>Oh, no no no. Consiela clear</div>
     @endforelse
