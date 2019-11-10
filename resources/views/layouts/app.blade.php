4a3b3844d6be202118ae1f715d6d28d667850999
Jimmy Basiluk <vasilukwolf@gmail.com>
2019-11-10 12:55:38 -0500

    grid project style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,9 +26,11 @@
         <nav class="bg-white">
             <div class="container mx-auto">
                 <div class="flex justify-between item-center py-2">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
+                    <h1>
+                      <a class="navbar-brand" href="{{ url('/') }}">
+                        <img src="/images/logo.svg" />
+                      </a>
+                    </h1>
                     <div>
                         <!-- Right Side Of Navbar -->
                         <ul class="navbar-nav ml-auto">
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,18 +1,23 @@
 @extends('layouts.app')
 @section('content')
-<div class="flex items-center mb-4">
-    <a href="/projects/create" class="button">New projects</a>
-</div>
-<div class="flex flex-wrap -mx-3">
-    @forelse($projects as $project)
-    <div class="w-1/3 px-4 pb-6">
-    <div class="bg-white rounded shadow p-5">
-        <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
-        <div class="text-grey">{{ Str::limit($project->description,100) }}</div>
+<header class="flex items-center mb-4">
+    <div class="flex justify-between w-full">
+        <h2 class="text-grey text-sm">My projects</h2>
+        <a href="/projects/create" class="button">New projects</a>
     </div>
-  </div>
+</header>
+<main class="lg:flex lg:flex-wrap -mx-3 py-4">
+    @forelse($projects as $project)
+    <div class="lg:w-1/3 px-4 pb-6">
+        <div class="bg-white rounded-lg shadow p-5">
+            <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-ligth pl-4">
+            <a href="{{ $project->path() }}" class="text-black no-underline">  {{ $project->title }}</a>
+            </h3>
+            <div class="text-grey">{{ Str::limit($project->description,100) }}</div>
+        </div>
+    </div>
     @empty
     <div>Oh, no no no. Consiela clear</div>
     @endforelse
-</div>
+</main>
 @endsection
