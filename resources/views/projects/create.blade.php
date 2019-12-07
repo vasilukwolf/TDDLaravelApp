34df960d862c1ddbfee1cfea4db79868eec18b35
Jimmy Basiluk <vasilukwolf@gmail.com>
2019-12-07 06:53:47 -0500

    Add ne form view

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,23 +1,47 @@
-@extends('layouts.app')
+@extends ('layouts.app')
+
 @section('content')
-  <form method="post" action="/projects">
-    @csrf
-      <h1 class="heading is-1">Create a projects</h1>
-    <div class="field">
-      <label class="title">Title</label>
-        <div class="control">
-          <input class="input" type="text" name="title" placeholder="title">
+    <form
+        method="POST"
+        action="/projects"
+        class="lg:w-1/2 lg:mx-auto bg-white p-6 md:py-12 md:px-16 rounded shadow"
+    >
+        @csrf
+
+        <h1 class="text-2xl font-normal mb-10 text-center">
+            Let’s start something new
+        </h1>
+
+        <div class="field mb-6">
+            <label class="label text-sm mb-2 block" for="title">Title</label>
+
+            <div class="control">
+                <input
+                    type="text"
+                    class="input bg-transparent border border-grey-light rounded p-2 text-xs w-full"
+                    name="title"
+                    placeholder="My next awesome project">
+            </div>
         </div>
-    </div>
-    <div class="field">
-      <label class="description">Title</label>
-        <div class="control">
-          <textarea class="textarea" name="description" placeholder="description"></textarea>
+
+        <div class="field mb-6">
+            <label class="label text-sm mb-2 block" for="description">Description</label>
+
+            <div class="control">
+                <textarea
+                    name="description"
+                    rows="10"
+                    class="textarea bg-transparent border border-grey-light rounded p-2 text-xs w-full"
+                    placeholder="I should start learning piano."></textarea>
+            </div>
         </div>
-    </div>
-    <div class="control">
-      <button  type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded">Submit project</button>
-      <a href="/projects"><button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded">Canel</button></a>
-    </div>
-  </form>
+
+        <div class="field">
+            <div class="control">
+                <button type="submit" class="button is-link mr-2">Create Project</button>
+
+                <a href="/projects">Cancel</a>
+            </div>
+        </div>
+    </form>
 @endsection
