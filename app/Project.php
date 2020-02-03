f60c845e96f67c7d69f5a6462b9453a8f478d533
Jimmy Basiluk <vasilukwolf@gmail.com>
2020-02-03 15:25:52 -0500

    Was changed

--- app/Project.php
+++ app/Project.php
@@ -37,9 +37,9 @@ class Project extends Model
         return  $this->tasks()->create(compact('body'));
     }
 
-    protected function activityChanges($description)
+    protected function activityChanges()
     {
-        if ($description === 'updated') {
+        if ($this->wasChanged()) {
             return [
                 'before' => array_except(array_diff($this->old, $this->getAttributes()), 'updated_at'),
                 'after' => array_except($this->getChanges(), 'updated_at')
@@ -52,7 +52,7 @@ class Project extends Model
     {
         $this->activity()->create([
             'description' => $description,
-            'changes' => $this->activityChanges($description)
+            'changes' => $this->activityChanges()
         ]);
     }
 
