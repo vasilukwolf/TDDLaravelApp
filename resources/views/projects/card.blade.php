69ba10b8f9f54bcf7806a7bf37cb7f0dc04a727d
Jimmy Basiluk <vasilukwolf@gmail.com>
2020-02-07 07:54:28 -0500

    Fix issuse with long crad for delete

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -2,7 +2,7 @@
         <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-ligth pl-4">
         <a href="{{ $project->path() }}" class="text-black no-underline">  {{ $project->title }}</a>
         </h3>
-        <div class="text-grey mb-4">{{ str_limit($project->description, 100) }}</div>
+        <div class="text-grey mb-4">{{ str_limit($project->description, 50) }}</div>
         <footer>
         <form method="POST" action="{{ $project->path() }}" class="text-right">
             @method('DELETE')
