7eb27f54795ae3bc97bf3c2a97ad33c260909a67
Jimmy Basiluk <vasilukwolf@gmail.com>
2020-02-08 07:08:08 -0500

    35 lesson|Not invite to deleted project

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -6,9 +6,10 @@ use App\Project;
 use Faker\Generator as Faker;
 
 $factory->define(Project::class, function (Faker $faker) {
-    return [
-      'title'=> $faker->sentence,
-      'description'=> $faker->paragraph,
-      'owner_id'=> factory(App\User::class),
-    ];
+    return array(
+        'title'=> $faker->sentence,
+        'description'=> $faker->paragraph,
+        'notes' => 'Foobar notes',
+        'owner_id'=> factory(App\User::class),
+    );
 });
