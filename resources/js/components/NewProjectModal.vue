6e56526ff9b6abc650b2f661597ed59f719c7b1b
Jimmy Basiluk <vasilukwolf@gmail.com>
2020-02-14 14:53:14 -0500

    Bug with modal window

--- resources/js/components/NewProjectModal.vue
+++ resources/js/components/NewProjectModal.vue
@@ -1,62 +1,91 @@
 <template>
-    <modal name="new-project" classes="p-10 rounded-lg bg-card" height="auto">
-        <h1 class="font-normal mb-16 text-center text-2xl">Let's start some new</h1>
+    <modal name="new-project" classes="p-10 bg-card rounded-lg" height="auto">
+        <h1 class="font-normal mb-16 text-center text-2xl">Let’s Start Something New</h1>
 
-        <div class="flex">
-            <div class="flex-1 mr-4">
-                <div class="mb-4">
-                    <label for="title" class="text-sm block mb-2">Title</label>
-                    <input type="text" id="title" class="border border-muted-light py-1 p-2 px-2 text-xs  w-full block rounded">
+        <form @submit.prevent="submit">
+            <div class="flex">
+                <div class="flex-1 mr-4">
+                    <div class="mb-4">
+                        <label for="title" class="text-sm block mb-2">Title</label>
+
+                        <input
+                            type="text"
+                            id="title"
+                            class="border p-2 text-xs block w-full rounded"
+                            :class="errors.title ? 'border-error' : 'border-muted-light'"
+                            v-model="form.title">
+
+                        <span class="text-xs italic text-error" v-if="errors.title" v-text="errors.title[0]"></span>
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="description" class="text-sm block mb-2">Description</label>
+
+                        <textarea id="description" class="border border-muted-light p-2 text-xs block w-full rounded" rows="7" v-model="form.description"></textarea>
+
+                        <span class="text-xs italic text-error" v-if="errors.description" v-text="errors.description[0]"></span>
+                    </div>
                 </div>
-                <div class="mb-4">
-                    <label for="description" class="text-sm block mb-2">Description</label>
-                    <textarea type="text" id="description" class="border border-muted-light py-1 px-2 text-xs  w-full block rounded" rows="5">
-                    </textarea>
+
+                <div class="flex-1 ml-4">
+                    <div class="mb-4">
+                        <label class="text-sm block mb-2">Need Some Tasks?</label>
+                        <input
+                            type="text"
+                            class="border border-muted-light mb-2 p-2 text-xs block w-full rounded"
+                            placeholder="Task 1"
+                            v-for="task in form.tasks"
+                            v-model="task.body">
+                    </div>
+
+                    <button type="button" class="inline-flex items-center text-xs" @click="addTask">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" class="mr-2">
+                            <g fill="none" fill-rule="evenodd" opacity=".307">
+                                <path stroke="#000" stroke-opacity=".012" stroke-width="0" d="M-3-3h24v24H-3z"></path>
+                                <path fill="#000" d="M9 0a9 9 0 0 0-9 9c0 4.97 4.02 9 9 9A9 9 0 0 0 9 0zm0 16c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7zm1-11H8v3H5v2h3v3h2v-3h3V8h-3V5z"></path>
+                            </g>
+                        </svg>
+
+                        <span>Add New Task Field</span>
+                    </button>
                 </div>
             </div>
-            <div class="flex-1 mr-4">
-                <div class="mb-4">
-                    <label  class="text-sm block mb-2">Need some super challenge Task</label>
-                    <input type="text"
-                           class="border border-muted-light py-1 px-2 text-xs p-2 w-full block rounded"
-                           placeholder="Change Task"
-                           v-for="task in tasks"
-                           v-model="tasks.value"
-                    >
-                </div>
 
-                <button class="inline-flex items-center text-xs" @click="addTask">
-                    <svg height="18" width="18" viewBox="0 0 512 512"  xmlns="http://www.w3.org/2000/svg">
-                        <path d="m256 0c-141.164062 0-256 114.835938-256 256s114.835938 256 256 256 256-114.835938 256-256-114.835938-256-256-256zm0 0"
-                              fill="#2196f3"/><path d="m256 512c-141.164062 0-256-114.835938-256-256s114.835938-256 256-256 256
-                               114.835938 256 256-114.835938 256-256 256zm0-480c-123.519531 0-224 100.480469-224 224s100.480469
-                                224 224 224 224-100.480469 224-224-100.480469-224-224-224zm0 0"/>
-                        <path d="m368 272h-224c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h224c8.832031
-                         0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/>
-                        <path d="m256 384c-8.832031 0-16-7.167969-16-16v-224c0-8.832031 7.167969-16 16-16s16
-                         7.167969 16 16v224c0 8.832031-7.167969 16-16 16zm0 0"/></svg>
-                    <span class="m-1">Add new task field</span>
-                </button>
-            </div>
-        </div>
-        <footer class="flex justify-end">
-            <button class="is-outlined button mr-4 " @click="$modal.hide('new-project')">Cancel</button>
-            <button class="button">Create project</button>
-        </footer>
+            <footer class="flex justify-end">
+                <button type="button" class="button is-outlined mr-4" @click="$modal.hide('new-project')">Cancel</button>
+                <button class="button">Create Project</button>
+            </footer>
+        </form>
     </modal>
 </template>
+
 <script>
     export default {
         data() {
             return {
-                tasks:[
-                    {value: ''}
+                form: {
+                    title: '',
+                    description: '',
+                    tasks: [
+                        { body: ''},
                     ]
-                };
+                },
+
+                errors: {}
+            };
+        },
+
+        methods: {
+            addTask() {
+                this.form.tasks.push({ value: '' });
             },
-        methods:{
-            addTask(){
-                this.tasks.push({value:''})
+
+            async submit() {
+                try {
+                    location = (await axios.post('/projects', this.form)).data.message;
+                } catch (error) {
+                    this.errors = error.response.data.errors;
+                }
             }
         }
     }
