b62032aa67848d444c48442a7902ba8689145760
Jimmy Basiluk <vasilukwolf@gmail.com>
2020-02-15 14:00:05 -0500

    43 lesson

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,29 +51,26 @@
                             @endif
                         @else
                             <theme-switcher></theme-switcher>
+                            <dropdown align="right" width="200px">
+                                <template v-slot:trigger>
+                                    <button
+                                        class="flex items-center text-default no-underline text-sm focus:outline-none"
+                                        v-pre
+                                    >
+                                        <img width="35"
+                                             class="rounded-full mr-3"
+                                             src="{{ gravatar_url(auth()->user()->email) }}">
+                                        </a>
+                                        {{ auth()->user()->name }}
+                                    </button>
+                                </template>
 
-                            <a
-                                class="flex items-center text-default no-underline text-sm"
-                                href="#" role="button"
-                                data-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false"
-                                v-pre
-                            >
-                                <img width="35"
-                                     class="rounded-full mr-3"
-                                     src="{{ gravatar_url(auth()->user()->email) }}">
-                            </a>
-                            <a class="dropdown-item"
-                                   href="{{ route('logout') }}"
-                                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-                                >
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                <form id="logout-form" method="POST" action="/logout">
                                     @csrf
+
+                                    <button type="submit" class="dropdown-menu-link w-full text-left">Logout</button>
                                 </form>
+                            </dropdown>
                         @endguest
                     </div>
                 </div>
